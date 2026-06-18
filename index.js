de1208d599fef89a2391c4df375db94ad340dc3f
= <=>
2026-06-18 10:55:03 +0600

    packages: uuid, method-override, path installed. view-engine set to ejs. views folder connected to access ejs. added method-override middleware, serving static css file to ejs from server to client, added url-encoded middleware to access the url encoded data.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
+const methodOverride = require("method-override");
 
-app.get("/", (req, res) => {
-  res.send("request working");
-});
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.listen(port, () => {
   console.log(`Listening at port: ${port}`);
